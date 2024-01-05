282f8ea2ff7ae52a436fce81f9fb34769959b3a0
Ivan Lazaric <ivan.lazaric1@gmail.com>
2024-01-05 23:04:29 +0000

    z

---
+++ src/experiments/to_free_function_pointer_example2_main.cpp
@@ -0,0 +1,7 @@
+void fn1();
+void fn2();
+
+int main(){
+  fn1();
+  fn2();
+}
---
+++ src/experiments/to_free_function_pointer_example2_tu1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "to_free_function_pointer.hpp"
+#include "to_free_function_pointer_example2_struct.hpp"
+
+void fn1(){
+  std::cout << (const void*)(ivl::to_free_function_pointer_v<&S::memfn>) << std::endl;
+}
