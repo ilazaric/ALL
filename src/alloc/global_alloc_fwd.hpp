6568792496a272f5956b3398a86c2ab8ef78f378
Ivan Lazaric <ivan.lazaric1@gmail.com>
2024-02-13 14:45:03 +0000

    z

---
+++ src/alloc/global_alloc_fwd.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ivl::alloc {
+
+  // forward declaration that is a customization point
+  // if using stuff that uses this (like ivl/stl/vector)
+  // the user needs to define the allocator as well
+  template<typename T>
+  struct GlobalAlloc;
+
+} // namespace ivl::alloc
