844a0f6858858ecf9a5b01b7d265d2fe8b186b6c
Ivan Lazaric <ivan.lazaric1@gmail.com>
2024-02-13 22:04:13 +0000

    z

--- src/debug/assert.hpp
+++ src/debug/assert.hpp
@@ -11,33 +11,32 @@
 #include <ivl/logger>
 
 #ifdef IVL_DBG_MODE
-# if __cpp_lib_stacktrace >= 202011L
-#  include <iostream>
-#  include <stacktrace>
-#  define IVL_DBG_ASSERT(expr, ...) do {                                  \
-    if (!(expr)) [[unlikely]] {                                       \
+
+#if __has_include(<stacktrace>)
+# include <stacktrace>
+# define IVL_DBG_ASSERT_STACKTRACE              \
+  std::cerr << "[IVL] stacktrace:\n"            \
+  << std::stacktrace::current() << "\n"
+#elif __has_include(<boost/stacktrace.hpp>)
+# include <boost/stacktrace.hpp>
+# define IVL_DBG_ASSERT_STACKTRACE              \
+  std::cerr << "[IVL] stacktrace:\n"            \
+  << boost::stacktrace::stacktrace() << "\n"
+#else
+# define IVL_DBG_ASSERT_STACKTRACE
+#endif
+
+#include <iostream>
+#define IVL_DBG_ASSERT(expr, ...) do {                  \
+    if (!(expr)) [[unlikely]] {                         \
       std::cerr << "[IVL] ERROR: ASSERTION FAILED\n"    \
-                <<"[IVL] LINE: " << __LINE__ << "\n"    \
-                << "[IVL] EXPR: " << #expr << "\n"      \
-                << std::stacktrace::current() << "\n";  \
+                << "[IVL] LINE: " << __LINE__ << "\n"   \
+                << "[IVL] EXPR: " << #expr << "\n";     \
+      IVL_DBG_ASSERT_STACKTRACE;                        \
       LOG("data" __VA_OPT__(,) __VA_ARGS__);            \
       exit(-1);                                         \
     }} while (false)
-# else
-#  include <iostream>
-#  define BOOST_STACKTRACE_USE_ADDR2LINE
-#  include <boost/stacktrace.hpp>
-#  define IVL_DBG_ASSERT(expr, ...) do {                                  \
-    if (!(expr)) [[unlikely]] {                                       \
-      std::cerr << "[IVL] ERROR: ASSERTION FAILED\n"    \
-                <<"[IVL] LINE: " << __LINE__ << "\n"    \
-                << "[IVL] EXPR: " << #expr << "\n";     \
-      std::cerr << "[IVL] stacktrace:\n"                                \
-                << boost::stacktrace::stacktrace() << std::endl;        \
-      LOG("data" __VA_OPT__(,) __VA_ARGS__);                            \
-      exit(-1);                                         \
-    }} while (false)
-# endif
-#else
-# define IVL_DBG_ASSERT(expr)
-#endif
+
+#else // IVL_DBG_MODE
+#define IVL_DBG_ASSERT(...)
+#endif // IVL_DBG_MODE
