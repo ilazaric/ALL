ca116f72fb72428c5b59fabb4b47383589844fa0
Ivan Lazaric <ivan.lazaric1@gmail.com>
2025-12-07 15:51:37 +0100

    generate syscall numbers from linux x86 tbl

---
+++ src/linux/syscall_numbers.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+// TODO: replace with ivl null terminated string view
+#include <string_view>
+
+namespace ivl::linux {
+
+  enum class syscall_number {
+#define X(nr, name) name = nr,
+#include <ivl/linux/syscall_numbers_X>
+  };
+
+  // If the argument is invalid returns default-constructed std::string_view.
+  constexpr std::string_view syscall_name(syscall_number nr) noexcept {
+    switch (nr) {
+    default:
+      return {};
+#define X(nr, name)                                                                                                    \
+  case syscall_number::name:                                                                                           \
+    return #name;
+#include <ivl/linux/syscall_numbers_X>
+    }
+  }
+
+  constexpr bool is_valid_syscall_number(int nr) noexcept {
+    switch (nr) {
+    default:
+      return false;
+#define X(nr, name)                                                                                                    \
+  case nr:                                                                                                             \
+    return true;
+#include <ivl/linux/syscall_numbers_X>
+    }
+  }
+
+} // namespace ivl::linux
