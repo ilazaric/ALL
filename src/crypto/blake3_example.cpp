9b89a08d3507588529030280588bd9a0964dee8c
Ivan Lazaric <ivan.lazaric1@gmail.com>
2025-12-08 23:30:59 +0100

    save

--- src/crypto/blake3_example.cpp
+++ src/crypto/blake3_example.cpp
@@ -1,17 +1,20 @@
 #include <ivl/crypto/blake3>
-#include <fstream>
+#include <ivl/linux/raw_generated_syscalls>
+#include <cassert>
 #include <iostream>
 #include <print>
-#include <sstream>
-
-std::string consume(auto&& in) {
-  std::stringstream buffer;
-  buffer << in.rdbuf();
-  return std::move(buffer).str();
-}
 
 int main(int argc, char** argv) {
-  auto contents = argc == 2 ? consume(std::ifstream(argv[1])) : consume(std::cin);
+  std::string contents;
+  {
+    char buf[1 << 16];
+    while (true) {
+      auto ret = ivl::linux::raw_syscalls::read(0, buf, sizeof(buf));
+      assert(ret >= 0);
+      if (ret == 0) break;
+      contents += std::string_view(buf, ret);
+    }
+  }
   auto hash = ivl::crypto::blake3::hash(contents);
   std::string_view bla((char*)&hash, (char*)(&hash + 1));
   for (auto c : bla) std::print("{:02x}", c);
