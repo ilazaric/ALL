3d6d3e22367f971b09ca013174cb590eec0d79ce
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-01-24 03:06:16 +0100

    save

--- ivl/reflection/json.hpp
+++ ivl/reflection/json.hpp
@@ -6,45 +6,48 @@
 #include <meta>
 
 namespace ivl {
+enum class from_to_json_impl_direction { FROM, TO };
 
-struct from_json_impl_tag {};
-struct to_json_impl_tag {};
-
-template <typename T, typename Tag>
-auto from_to_json_impl(Tag tag, const auto& arg) {
+template <
+  typename T, from_to_json_impl_direction Direction,
+  typename InputT = std::conditional_t<Direction == from_to_json_impl_direction::TO, T, nlohmann::json>,
+  typename RetT = std::conditional_t<Direction == from_to_json_impl_direction::TO, nlohmann::json, T>>
+RetT from_to_json_impl(const InputT& arg) {
   static_assert(!is_pointer_type(^^T));
 
-  using RetT = std::conditional_t<std::same_as<Tag, to_json_impl_tag>, nlohmann::json, T>;
+  using enum from_to_json_impl_direction;
 
-  if constexpr (!is_class_type(^^T) || std::same_as<T, nlohmann::json> || std::same_as<T, std::string>) {
-    if constexpr (std::same_as<Tag, to_json_impl_tag>) return nlohmann::json(arg);
+  if constexpr (std::same_as<T, std::filesystem::path>) {
+    return from_to_json_impl<std::string, Direction>(arg);
+  } else if constexpr (!is_class_type(^^T) || std::same_as<T, nlohmann::json> || std::same_as<T, std::string>) {
+    if constexpr (Direction == TO) return nlohmann::json(arg);
     else return arg.template get<T>();
   } else if constexpr (reflection::is_instantiation_of(^^T, ^^std::vector)) {
     using ElementT = [:template_arguments_of(^^T)[0]:];
     auto ret = RetT{};
-    if constexpr (std::same_as<Tag, to_json_impl_tag>) ret = nlohmann::json::array();
-    for (auto&& el : arg) ret.emplace_back(from_to_json_impl<ElementT>(tag, el));
+    if constexpr (Direction == TO) ret = nlohmann::json::array();
+    for (auto&& el : arg) ret.emplace_back(from_to_json_impl<ElementT, Direction>(el));
     return ret;
   } else if constexpr (reflection::is_instantiation_of(^^T, ^^std::map)) {
     using KeyT = [:template_arguments_of(^^T)[0]:];
     using ValueT = [:template_arguments_of(^^T)[1]:];
     auto ret = RetT{};
-    if constexpr (std::same_as<Tag, to_json_impl_tag>) ret = nlohmann::json::object();
+    if constexpr (Direction == TO) ret = nlohmann::json::object();
     for (auto&& [key, value] : arg) {
-      ret.emplace(from_to_json_impl<KeyT>(tag, key), from_to_json_impl<ValueT>(tag, value));
+      ret.emplace(from_to_json_impl<KeyT, Direction>(key), from_to_json_impl<ValueT, Direction>(value));
     }
     return ret;
   } else if constexpr (is_class_type(^^T) && !reflection::is_child_of(^^T, ^^std) &&
                        !reflection::is_child_of(^^T, ^^nlohmann)) {
     auto ret = RetT{};
-    if constexpr (std::same_as<Tag, to_json_impl_tag>) ret = nlohmann::json::object();
+    if constexpr (Direction == TO) ret = nlohmann::json::object();
     [:substitute(
         ^^reflection::foreach, reflection::wrap(nonstatic_data_members_of(^^T, std::meta::access_context::unchecked()))
       ):]()([&]<std::meta::info member> {
-      if constexpr (std::same_as<Tag, to_json_impl_tag>) {
-        ret.emplace(identifier_of(member), from_to_json_impl<typename[:type_of(member):]>(tag, arg.[:member:]));
+      if constexpr (Direction == TO) {
+        ret.emplace(identifier_of(member), from_to_json_impl<typename[:type_of(member):], Direction>(arg.[:member:]));
       } else {
-        ret.[:member:] = from_to_json_impl<typename[:type_of(member):]>(tag, arg[identifier_of(member)]);
+        ret.[:member:] = from_to_json_impl<typename[:type_of(member):], Direction>(arg[identifier_of(member)]);
       }
     });
     return ret;
@@ -55,39 +58,11 @@ auto from_to_json_impl(Tag tag, const auto& arg) {
 
 template <typename T>
 T from_json(const nlohmann::json& j) {
-  return from_to_json_impl<T>(from_json_impl_tag{}, j);
+  return from_to_json_impl<T, from_to_json_impl_direction::FROM>(j);
 }
 
 template <typename T>
 nlohmann::json to_json(const T& t) {
-  return from_to_json_impl<T>(to_json_impl_tag{}, t);
+  return from_to_json_impl<T, from_to_json_impl_direction::TO>(t);
 }
-
-// template <typename T>
-// nlohmann::json to_json(const T& arg) {
-//   static_assert(!is_pointer_type(^^T));
-
-//   if constexpr (!is_class_type(^^T) || std::same_as<T, nlohmann::json> || std::same_as<T, std::string>) {
-//     return nlohmann::json(arg);
-//   } else if constexpr (is_instantiation_of(^^T, ^^std::vector)) {
-//     auto ret = nlohmann::json::array();
-//     for (auto&& el : arg) ret.emplace_back(to_json(el));
-//     return ret;
-//   } else if constexpr (is_instantiation_of(^^T, ^^std::map)) {
-//     auto ret = nlohmann::json::object();
-//     for (auto&& [key, value] : arg) {
-//       auto kj = to_json(key);
-//       ret.emplace(kj.is_string() ? kj.template get<std::string>() : kj.dump(), to_json(value));
-//     }
-//     return ret;
-//   } else if constexpr (is_class_type(^^T) && !is_child_of(^^T, ^^std) && !is_child_of(^^T, ^^nlohmann)) {
-//     auto ret = nlohmann::json::object();
-//     [:substitute(^^foreach, wrap(nonstatic_data_members_of(^^T, std::meta::access_context::unchecked()))):]()(
-//       [&]<std::meta::info member> { ret.emplace(identifier_of(member), to_json(arg.[:member:])); }
-//     );
-//     return ret;
-//   } else {
-//     static_assert(false);
-//   }
-// }
 } // namespace ivl
