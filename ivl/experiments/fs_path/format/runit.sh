6b523c210541b3ea5b958564d6ee508bd9812caa
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-02-10 01:08:53 +0100

    cleanup benchmark script

--- ivl/experiments/fs_path/format/runit.sh
+++ ivl/experiments/fs_path/format/runit.sh
@@ -5,8 +5,17 @@ cd "$(dirname $(realpath "$0"))"
 set -euo pipefail
 set -x
 
-g++ -Wl,-rpath=/opt/GCC/lib64 -DFORMAT_VERSION='"ilazaric.hpp"' -O3 -std=c++23 bench_format.cpp -o bench_format.ilazaric -lbenchmark
-g++ -Wl,-rpath=/opt/GCC/lib64 -DFORMAT_VERSION='"jwakely.hpp"' -O3 -std=c++23 bench_format.cpp -o bench_format.jwakely -lbenchmark
+versions=(ilazaric jwakely)
 
-echo "\nilazaric version\n" && taskset -c 1 ./bench_format.ilazaric --benchmark_min_warmup_time=2 --benchmark_repetitions=10
-echo "\njwakely  version\n" && taskset -c 1 ./bench_format.jwakely  --benchmark_min_warmup_time=2 --benchmark_repetitions=10
+for version in ${versions[@]}
+do
+    g++ -Wl,-rpath=/opt/GCC/lib64 -DFORMAT_VERSION="\"$version.hpp\"" -O3 -std=c++23 bench_format.cpp -o bench_format.$version -lbenchmark
+done
+
+for version in ${versions[@]}
+do
+    echo
+    echo "$version version"
+    echo
+    taskset -c 1 ./bench_format.$version --benchmark_min_warmup_time=2 --benchmark_repetitions=10
+done
