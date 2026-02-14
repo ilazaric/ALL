24d242bac8465ffb72488faa9e2815ea10c90f48
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-02-14 16:32:02 +0100

    remove unused

--- ivl/experiments/gcc-qual/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-set -x
-
-g++ -Wl,-rpath=/opt/GCC/lib64 -std=c++26 -O3 -freflection \
-    -xc++ -include "$1.cpp" -include lib.hpp /dev/null \
-    -o "$1"
