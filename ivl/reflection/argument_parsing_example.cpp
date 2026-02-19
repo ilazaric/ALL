c4f934fc12a31805e8f9d76261bd19a9fbdb2a41
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-02-19 01:38:28 +0100

    save dead code

---
+++ ivl/reflection/argument_parsing_example.cpp
@@ -0,0 +1,37 @@
+// #include <ivl/reflection/argument_parsing>
+
+// [[=ivl::description("foo")]]
+// struct cc_bundle {
+//   bool bla;
+//   std::string truc;
+// };
+
+// struct args_bundle {
+
+// };
+
+// #include <meta>
+
+// int main();
+
+// namespace hideme {}
+
+int main() {
+  return 42;
+}
+
+// int [: mi :] () {
+//   return 42;
+// }
+
+// typename
+// [:
+//  type_of(mi)
+//  :]
+// [: mi :] {
+//   return 42;
+// }
+
+// int main(const args_bundle& args) {
+  
+// }
