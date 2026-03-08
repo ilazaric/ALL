c3f6d1c1022dd9bed626f08592de9097df4a8d5f
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-03-08 20:38:17 +0100

    meta::info formatting

--- ivl/reflection/utility.hpp
+++ ivl/reflection/utility.hpp
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <ivl/reflection/prettier_types>
+#include <ivl/utility>
 #include <cassert>
 #include <meta>
+
+// TODO: maybe move to ivl/reflection/format , add prettier_types to it
+// doesnt work with std::print
+template <>
+struct std::formatter<std::meta::info, char> {
+  std::formatter<std::string_view, char> underlying;
+
+  consteval auto parse(auto& ctx) { return underlying.parse(ctx); }
+
+  consteval void set_debug_format() noexcept { underlying.set_debug_format(); }
+
+  consteval auto format(std::meta::info i, auto& ctx) const;
+};
 
 namespace ivl::reflection {
 consteval bool is_instantiation_of(std::meta::info typei, std::meta::info templatei) {
@@ -28,9 +43,29 @@ consteval auto nsdms(std::meta::info i) {
   return define_static_array(nonstatic_data_members_of(i, std::meta::access_context::unchecked()));
 }
 
+consteval auto members(std::meta::info i) {
+  return define_static_array(members_of(i, std::meta::access_context::unchecked()));
+}
+
 // constexpr template for is broken at the moment, this can be used to avoid it.
 template <std::meta::info... Is>
 auto foreach () {
   return [](auto&& callable) { ((callable.template operator()<Is>()), ...); };
 }
+
+consteval std::meta::info get_nsdm(std::meta::info i, std::string_view name) {
+  i = remove_cvref(dealias(i));
+  for (auto member : nsdms(i))
+    if (has_identifier(member) && identifier_of(member) == name) return member;
+  panic("no member with name {:?} found in {:?}", name, i);
+}
 } // namespace ivl::reflection
+
+consteval auto std::formatter<std::meta::info, char>::format(std::meta::info i, auto& ctx) const {
+  auto fmt_cpy = underlying;
+  auto& fmt_str = fmt_cpy.[:ivl::reflection::get_nsdm(^^decltype(fmt_cpy), "_M_f"):];
+  auto& spec = fmt_str.[:ivl::reflection::get_nsdm(^^decltype(fmt_str), "_M_spec"):];
+  bool is_debug = spec._M_debug;
+  spec._M_debug = 0;
+  return fmt_cpy.format(is_debug ? ivl::reflection::display_string_of(i) : identifier_of(i), ctx);
+}
