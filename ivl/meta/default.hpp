3b5e28643711417afafe622746b7e473a2b7767a
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-04-08 06:26:38 +0200

    reformat & cleanup meta

--- ivl/meta/default.hpp
+++ ivl/meta/default.hpp
@@ -10,31 +10,34 @@
 namespace ivl::meta {
 
 // TODO: when we get universal template params, think how stuff can improve here
-template <typename...>
+template<typename...>
 struct type_list {};
 
-template <typename T>
+template<typename T>
 struct tag {
   using type = T;
 };
 
-template <typename T, typename... Ts>
+template<typename T, typename... Ts>
 concept same_as_one_of = (std::same_as<T, Ts> || ...);
 
 // Are all types different?
-template <typename...>
-struct is_unique;
-template <>
-struct is_unique<> {
+template<typename...>
+struct is_unique_impl;
+template<>
+struct is_unique_impl<> {
   inline static constexpr bool value = true;
 };
-template <typename Head, typename... Tail>
-struct is_unique<Head, Tail...> {
-  inline static constexpr bool value = (true && ... && !std::same_as<Head, Tail>) && is_unique<Tail...>::value;
+template<typename Head, typename... Tail>
+struct is_unique_impl<Head, Tail...> {
+  inline static constexpr bool value = (true && ... && !std::same_as<Head, Tail>) && is_unique_impl<Tail...>::value;
 };
 
-template <typename... Ts>
-inline constexpr bool is_unique_v = is_unique<Ts...>::value;
+template<typename... Ts>
+concept is_unique = is_unique_impl<Ts...>::value;
+
+template<typename T>
+concept is_trivially_destructible = std::is_trivially_destructible_v<T>;
 
 // TODO: add callable(index) variant
 // UPDT: probably actually just remove this
@@ -42,15 +45,15 @@ void repeat(size_t n, auto&& callable) {
   for (size_t i = 0; i < n; ++i) callable();
 }
 
-template <typename T>
+template<typename T>
 struct construct_t {
   static constexpr T operator()(auto&&... args) { return T(std::forward<decltype(args)>(args)...); }
 };
-template <typename T>
+template<typename T>
 inline constexpr construct_t<T> construct;
 
 // don't put this on the heap
-template <typename T>
+template<typename T>
 union uninitialized {
   T data;
   uninitialized() {}
