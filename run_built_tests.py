bd1ff3e58ea8f564680ec742a92cd41f41be10e9
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-04-15 00:28:28 +0200

    start running some tests in CI

---
+++ run_built_tests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import glob
+from pathlib import Path
+import subprocess
+
+repo_root = Path(__file__).parent.resolve()
+ivl_dir = repo_root / "ivl"
+
+tests = glob.glob(f"{ivl_dir}/**/*@test", recursive=True)
+print(f"[RUNNER] test: {tests}")
+
+failed = []
+for test in tests:
+    print(f"[RUNNER] running test {test} ...")
+    p = subprocess.run([test])
+    if p.returncode == 0:
+        continue
+    print(f"[RUNNER] test {test} failed :(")
+    failed.append(test)
+
+if len(failed) == 0:
+    exit(0)
+
+print(f"[RUNNER] seen failures: {failed}")
+exit(1)
