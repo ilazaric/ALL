9e7ab0c133986b07db1acd09cb52dd4af0086d3c
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-05-18 15:27:37 +0200

    cleanup

--- ivl/parsing/ninja_example.cpp
+++ ivl/parsing/ninja_example.cpp
@@ -3,7 +3,5 @@
 
 int ivl_main(const std::filesystem::path& p) {
   std::println("{}", ivl::to_json(ivl::parsing::ninja::parse(p)).dump(2));
-  // ivl::parsing::ninja::rule_variable::text foo = "bar";
-  // std::println("{}", ivl::to_json(foo).dump(2));
   return 0;
 }
