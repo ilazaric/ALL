aa5d6b901bc39e277f4cbe4690d7b218e9a86b12
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-06-04 08:23:57 +0200

    save

--- ivl/thomson/eval.hpp
+++ ivl/thomson/eval.hpp
@@ -18,3 +18,19 @@ double evaluate_for(std::span<const point> points, std::size_t j) {
     if (i != j) ret += evaluate(points[i], points[j]);
   return ret;
 }
+
+double evaluate_assume_normed(point a, point b) { return 1 / distance(a, b); }
+
+double evaluate_assume_normed(std::span<const point> points) {
+  double ret = 0;
+  for (std::size_t i = 0; i < points.size(); ++i)
+    for (std::size_t j = 0; j < i; ++j) ret += evaluate_assume_normed(points[i], points[j]);
+  return ret;
+}
+
+double evaluate_assume_normed_for(std::span<const point> points, std::size_t j) {
+  double ret = 0;
+  for (std::size_t i = 0; i < points.size(); ++i)
+    if (i != j) ret += evaluate_assume_normed(points[i], points[j]);
+  return ret;
+}
