cf0226b597976d73e1a452925b34b398a5f34606
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-06-14 13:41:48 +0200

    save

--- ivl/thomson/linalg.hpp
+++ ivl/thomson/linalg.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ivl/meta>
+#include <memory>
 #include <span>
 #include <variant>
 #include <vector>
@@ -27,19 +29,26 @@
 // * linear operator
 
 struct shape_t {
-  struct field_t {};
+  struct field_t {
+    auto operator<=>(const field_t&) const = default;
+  };
   struct vector_t {
     std::size_t n;
+    auto operator<=>(const vector_t&) const = default;
   };
   struct cartesian_t {
     std::vector<shape_t> shapes;
+    auto operator<=>(const cartesian_t&) const = default;
   };
   struct tensor_t {
     std::vector<shape_t> shapes;
+    auto operator<=>(const tensor_t& o) const = default;
   };
 
   std::size_t total_size;
   std::variant<field_t, vector_t, cartesian_t, tensor_t> data;
+
+  auto operator<=>(const shape_t&) const = default;
 
   bool is_field() const { return std::holds_alternative<field_t>(data); }
   bool is_vector() const { return std::holds_alternative<vector_t>(data); }
@@ -53,15 +62,30 @@ struct shape_t {
     contract_assert(self.is_cartesian_product());
     return std::forward_like<decltype(self)>(std::get<cartesian_t>(self.data).shapes);
   }
-  decltype(auto) linear_operator_elements(this auto&& self) {
+  decltype(auto) linear_operator_from(this auto&& self) {
     // not pre bc of gcc bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=125733
     contract_assert(self.is_linear_operator());
-    return std::forward_like<decltype(self)>(std::get<tensor_t>(self.data).shapes);
+    return std::forward_like<decltype(self)>(std::get<tensor_t>(self.data).shapes[0]);
+  }
+  decltype(auto) linear_operator_to(this auto&& self) {
+    // not pre bc of gcc bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=125733
+    contract_assert(self.is_linear_operator());
+    return std::forward_like<decltype(self)>(std::get<tensor_t>(self.data).shapes[1]);
   }
   decltype(auto) vector_size(this auto&& self) {
     // not pre bc of gcc bug: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=125733
     contract_assert(self.is_vector());
     return std::forward_like<decltype(self)>(std::get<vector_t>(self.data).n);
+  }
+  decltype(auto) final_to(this auto&& self) {
+    auto* curr = &self;
+    while (curr->is_linear_operator()) curr = &curr->linear_operator_to();
+    return std::forward_like<decltype(self)>(*curr);
+  }
+  std::size_t linear_operator_rank() const {
+    std::size_t ret = 0;
+    for (auto* curr = this; curr->is_linear_operator(); ++ret, curr = &curr->linear_operator_to());
+    return ret;
   }
 
   template<auto Member>
@@ -73,11 +97,7 @@ struct shape_t {
       for (auto&& el : cartesian_product_elements()) total_size += (el.*Member)();
       return total_size;
     }
-    if (is_linear_operator()) {
-      total_size = 1;
-      for (auto&& el : linear_operator_elements()) total_size *= (el.*Member)();
-      return total_size;
-    }
+    if (is_linear_operator()) return total_size = (linear_operator_from().*Member)() * (linear_operator_to().*Member)();
     contract_assert(false);
   }
 
@@ -87,8 +107,7 @@ struct shape_t {
   static shape_t field() { return shape_t{.total_size = 1, .data{field_t{}}}; }
   static shape_t vector(std::size_t n) { return shape_t{.total_size = n, .data{vector_t{n}}}; }
 
-  template<typename... Ts>
-    requires(std::same_as<Ts, shape_t> && ...)
+  template<ivl::meta::is_unqualified<shape_t>... Ts>
   static shape_t cartesian_product(Ts&&... args) {
     return shape_t{
       .total_size = (args.size() + ...),
@@ -96,20 +115,40 @@ struct shape_t {
     };
   }
 
-  template<typename... Ts>
-    requires(std::same_as<Ts, shape_t> && ...)
-  static shape_t linear_operator(Ts&&... args) {
+  template<ivl::meta::is_unqualified<shape_t> T>
+  static shape_t linear_operator(T&& from) {
+    return std::forward<T>(from);
+  }
+
+  template<ivl::meta::is_unqualified<shape_t> T, ivl::meta::is_unqualified<shape_t> U>
+  static shape_t linear_operator(T&& from, U&& to) {
     return shape_t{
-      .total_size = (args.size() * ...),
-      .data{tensor_t{std::forward<decltype(args)>(args)...}},
+      .total_size = from.size() * to.size(),
+      .data{tensor_t{.shapes{
+        std::forward<T>(from),
+        std::forward<T>(to),
+      }}},
     };
+  }
+
+  template<ivl::meta::is_unqualified<shape_t> T, ivl::meta::is_unqualified<shape_t>... Us>
+    requires(sizeof...(Us) >= 2)
+  static shape_t linear_operator(T&& from, Us&&... to) {
+    return linear_operator(std::forward<T>(from), linear_operator(std::forward<Us>(to)...));
   }
 };
 
-struct ranked {
-  std::size_t index;
-  std::size_t rank;
-};
+// std::strong_ordering shape_t::tensor_t::operator<=>(const shape_t::tensor_t& o) const {
+//   return std::tie(*from, *to) <=> std::tie(*o.from, *o.to);
+// }
+// bool shape_t::tensor_t::operator==(const shape_t::tensor_t& o) const {
+//   return std::tie(*from, *to) == std::tie(*o.from, *o.to);
+// }
+
+// struct ranked {
+//   std::size_t index;
+//   std::size_t rank;
+// };
 
 // TODO: different shape might make more sense
 // ....: instead of {extent, extent, ...}
@@ -121,71 +160,76 @@ struct ranked {
 
 // invariant: data.size() == (shape[i] * ...)
 struct mdarray_cref {
-  std::span<const std::size_t> shape;
+  // std::span<const std::size_t> shape;
+  const shape_t* shape;
   std::span<const double> data;
 
-  std::size_t rank() const { return shape.size(); }
+  // std::size_t rank() const { return shape.size(); }
   std::size_t size() const { return data.size(); }
-  std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
+  // std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
 
-  double extract_number() const pre(rank() == 0) {
+  double extract_number() const pre(shape->is_field()) {
     contract_assert(size() == 1);
     return data[0];
   }
 
-  mdarray_cref operator[](std::size_t i) const pre(rank() > 0 && i < extent(0)) {
-    std::size_t foo = data.size() / shape[0];
-    return mdarray_cref{shape.subspan(1), data.subspan(i * foo, foo)};
+  mdarray_cref operator[](std::size_t i) const pre(
+    shape->is_linear_operator() && i < shape->linear_operator_from().size()
+    // rank() > 0 && i < extent(0)
+  ) {
+    std::size_t foo = shape->linear_operator_to().size();
+    return mdarray_cref{&shape->linear_operator_to(), data.subspan(i * foo, foo)};
   }
 
-  mdarray_cref operator[](ranked r) const pre(rank() >= r.rank) {
-    std::size_t prefix = 1;
-    for (std::size_t i = 0; i < r.rank; ++i) prefix *= shape[i];
-    contract_assert(r.index < prefix);
-    std::size_t foo = data.size() / prefix;
-    return mdarray_cref{shape.subspan(r.rank), data.subspan(r.index * foo, foo)};
-  }
+  // mdarray_cref operator[](ranked r) const pre(rank() >= r.rank) {
+  //   std::size_t prefix = 1;
+  //   for (std::size_t i = 0; i < r.rank; ++i) prefix *= shape[i];
+  //   contract_assert(r.index < prefix);
+  //   std::size_t foo = data.size() / prefix;
+  //   return mdarray_cref{shape.subspan(r.rank), data.subspan(r.index * foo, foo)};
+  // }
 };
 
 // invariant: data.size() == (shape[i] * ...)
 struct mdarray_ref {
-  std::span<const std::size_t> shape;
+  // std::span<const std::size_t> shape;
+  const shape_t* shape;
   std::span<double> data;
 
-  std::size_t rank() const { return shape.size(); }
+  // std::size_t rank() const { return shape.size(); }
   std::size_t size() const { return data.size(); }
-  std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
+  // std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
 
-  double extract_number() const pre(rank() == 0) {
+  double extract_number() const pre(shape->is_field()) {
     contract_assert(size() == 1);
     return data[0];
   }
 
-  mdarray_ref operator[](std::size_t i) const pre(rank() > 0 && i < extent(0)) {
-    std::size_t foo = data.size() / shape[0];
-    return mdarray_ref{shape.subspan(1), data.subspan(i * foo, foo)};
+  mdarray_ref operator[](std::size_t i) const pre(
+    shape->is_linear_operator() && i < shape->linear_operator_from().size()
+    // rank() > 0 && i < extent(0)
+  ) {
+    std::size_t foo = shape->linear_operator_to().size();
+    return mdarray_ref{&shape->linear_operator_to(), data.subspan(i * foo, foo)};
   }
 
-  mdarray_ref operator[](ranked r) const pre(rank() >= r.rank) {
-    std::size_t prefix = 1;
-    for (std::size_t i = 0; i < r.rank; ++i) prefix *= shape[i];
-    contract_assert(r.index < prefix);
-    std::size_t foo = data.size() / prefix;
-    return mdarray_ref{shape.subspan(r.rank), data.subspan(r.index * foo, foo)};
-  }
+  // mdarray_ref operator[](ranked r) const pre(rank() >= r.rank) {
+  //   std::size_t prefix = 1;
+  //   for (std::size_t i = 0; i < r.rank; ++i) prefix *= shape[i];
+  //   contract_assert(r.index < prefix);
+  //   std::size_t foo = data.size() / prefix;
+  //   return mdarray_ref{shape.subspan(r.rank), data.subspan(r.index * foo, foo)};
+  // }
 
-  double& operator=(double x) pre(rank() == 0) {
+  double& operator=(double x) pre(shape->is_field()) {
     contract_assert(data.size() == 1);
     return data[0] = x;
   }
 };
 
-struct shaped_t {};
-inline constexpr shaped_t shaped;
-
 // invariant: data.size() == (shape[i] * ...)
 struct mdarray {
-  std::vector<std::size_t> shape;
+  shape_t shape;
   std::vector<double> data;
 
   // 0,0,...,0,0 -> 0
@@ -201,70 +245,72 @@ struct mdarray {
   mdarray& operator=(mdarray&&) = default;
   ~mdarray() = default;
 
-  std::size_t rank() const { return shape.size(); }
+  // std::size_t rank() const { return shape.size(); }
   std::size_t size() const { return data.size(); }
-  std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
+  // std::size_t extent(std::size_t i) const pre(i < rank()) { return shape[i]; }
 
   bool check_size_invariant() const {
-    std::size_t shape_size = 1;
-    for (auto extent : shape) {
-      bool overflow = __builtin_mul_overflow(shape_size, extent, &shape_size);
-      if (overflow) return false;
-    }
-    return shape_size == data.size();
+    // std::size_t shape_size = 1;
+    // for (auto extent : shape) {
+    //   bool overflow = __builtin_mul_overflow(shape_size, extent, &shape_size);
+    //   if (overflow) return false;
+    // }
+    // return shape_size == data.size();
+    return shape.size() == data.size();
   }
 
   void match_size() {
-    std::size_t sz = 1;
-    for (std::size_t i = 0; i < rank(); ++i) {
-      bool overflow = __builtin_mul_overflow(sz, extent(i), &sz);
-      contract_assert(!overflow);
-    }
-    data.resize(sz, 0.0);
+    // std::size_t sz = 1;
+    // for (std::size_t i = 0; i < rank(); ++i) {
+    //   bool overflow = __builtin_mul_overflow(sz, extent(i), &sz);
+    //   contract_assert(!overflow);
+    // }
+    data.resize(shape.size(), 0.0);
     contract_assert(check_size_invariant());
   }
 
-  explicit mdarray(shaped_t, auto... extents) : shape{(std::size_t)extents...} {
+  explicit mdarray(shape_t shape) : shape(std::move(shape)) {
     // TODO: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=125645
-    contract_assert(((extents > 0) && ...));
+    // contract_assert(((extents > 0) && ...));
     match_size();
   }
 
-  double extract_number() const pre(rank() == 0) {
+  double extract_number() const pre(shape.is_field()) {
     contract_assert(data.size() == 1);
     return data[0];
   }
 
-  std::vector<double> extract_vector() const pre(rank() == 1) { return data; }
+  std::vector<double> extract_vector() const pre(shape.is_vector()) { return data; }
 
   // e_i --> data[Li...L(i+1)] (L == size / extent(0))
-  mdarray operator()(std::span<const double> vec) const pre(rank() > 0 && extent(0) == vec.size()) {
-    std::size_t n = vec.size();
-    mdarray ret;
-    for (std::size_t i = 1; i < rank(); ++i) ret.shape.push_back(extent(i));
-    ret.match_size();
+  mdarray
+  operator()(const mdarray& arg) const pre(shape.is_linear_operator() && shape.linear_operator_from() == arg.shape) {
+    std::size_t n = arg.size();
+    mdarray ret(shape.linear_operator_to());
+    // for (std::size_t i = 1; i < rank(); ++i) ret.shape.push_back(extent(i));
+    // ret.match_size();
     auto rem = ret.size();
     for (std::size_t jk = 0; jk < data.size(); ++jk) {
       std::size_t j = jk % rem;
       std::size_t k = jk / rem;
-      ret.data[j] += data[jk] * vec[k];
+      ret.data[j] += data[jk] * arg.data[k];
     }
     return ret;
   }
 
-  mdarray_ref as_ref() { return mdarray_ref{.shape{shape}, .data{data}}; }
-  mdarray_cref as_cref() const { return mdarray_cref{.shape{shape}, .data{data}}; }
+  mdarray_ref as_ref() { return mdarray_ref{.shape{&shape}, .data{data}}; }
+  mdarray_cref as_cref() const { return mdarray_cref{.shape{&shape}, .data{data}}; }
 
   operator mdarray_ref() { return as_ref(); }
   operator mdarray_cref() const { return as_cref(); }
 
   mdarray_ref operator[](std::size_t i) { return as_ref()[i]; }
-  mdarray_ref operator[](ranked r) { return as_ref()[r]; }
+  // mdarray_ref operator[](ranked r) { return as_ref()[r]; }
 
   mdarray_cref operator[](std::size_t i) const { return as_cref()[i]; }
-  mdarray_cref operator[](ranked r) const { return as_cref()[r]; }
+  // mdarray_cref operator[](ranked r) const { return as_cref()[r]; }
 
-  double& operator=(double x) pre(rank() == 0) {
+  double& operator=(double x) pre(shape.is_field()) {
     contract_assert(data.size() == 1);
     return data[0] = x;
   }
@@ -277,24 +323,25 @@ struct mdarray {
 // a : X -> Y -> Z (-> F)
 // b : Z -> W -> T (-> F)
 // compose(a,b) : X -> Y -> W -> T (-> F)
-mdarray compose(mdarray_cref a, mdarray_cref b) {
-  // LOG(std::format("{}", a.shape));
-  // LOG(std::format("{}", b.shape));
-  // TODO: pre
-  contract_assert(a.rank() > 0);
-  contract_assert(b.rank() > 0);
-  contract_assert(a.extent(a.rank() - 1) == b.extent(0));
-  mdarray ret;
-  for (std::size_t i = 0; i < a.rank() - 1; ++i) ret.shape.push_back(a.extent(i));
-  for (std::size_t i = 1; i < b.rank(); ++i) ret.shape.push_back(b.extent(i));
-  ret.match_size();
-  std::size_t overlap = b.extent(0);
-  std::size_t a_ex = a.size() / overlap;
-  std::size_t b_ex = b.size() / overlap;
-  // TODO: perf
-  for (std::size_t ai = 0; ai < a_ex; ++ai)
-    for (std::size_t ov = 0; ov < overlap; ++ov)
-      for (std::size_t bi = 0; bi < b_ex; ++bi)
-        ret.data[ai * b_ex + bi] += a.data[ai * overlap + ov] * b.data[ov * b_ex + bi];
-  return ret;
-}
+mdarray compose(mdarray_cref a, mdarray_cref b);
+// {
+//   // LOG(std::format("{}", a.shape));
+//   // LOG(std::format("{}", b.shape));
+//   // TODO: pre
+//   contract_assert(a.rank() > 0);
+//   contract_assert(b.rank() > 0);
+//   contract_assert(a.extent(a.rank() - 1) == b.extent(0));
+//   mdarray ret;
+//   for (std::size_t i = 0; i < a.rank() - 1; ++i) ret.shape.push_back(a.extent(i));
+//   for (std::size_t i = 1; i < b.rank(); ++i) ret.shape.push_back(b.extent(i));
+//   ret.match_size();
+//   std::size_t overlap = b.extent(0);
+//   std::size_t a_ex = a.size() / overlap;
+//   std::size_t b_ex = b.size() / overlap;
+//   // TODO: perf
+//   for (std::size_t ai = 0; ai < a_ex; ++ai)
+//     for (std::size_t ov = 0; ov < overlap; ++ov)
+//       for (std::size_t bi = 0; bi < b_ex; ++bi)
+//         ret.data[ai * b_ex + bi] += a.data[ai * overlap + ov] * b.data[ov * b_ex + bi];
+//   return ret;
+// }
--- ivl/thomson/linalg_test.cpp
+++ ivl/thomson/linalg_test.cpp
@@ -7,9 +7,9 @@
 // IVL test_only()
 
 [[= ivl::test]] void test_fib1() {
-  mdarray m(shaped, 2, 2);
+  mdarray m(shape_t::linear_operator(shape_t::vector(2), shape_t::vector(2)));
   m[1][1] = m[1][0] = m[0][1] = 1;
-  mdarray v(shaped, 2);
+  mdarray v(shape_t::vector(2));
   v[1] = 1;
   for (int i = 0; i < 10; ++i) v = compose(m, v);
   contract_assert((int)std::round(v[0].extract_number()) == 55);
@@ -17,9 +17,9 @@
 }
 
 [[= ivl::test]] void test_fib2() {
-  mdarray m(shaped, 2, 2);
+  mdarray m(shape_t::linear_operator(shape_t::vector(2), shape_t::vector(2)));
   m[1][1] = m[1][0] = m[0][1] = 1;
-  mdarray n(shaped, 2, 2);
+  mdarray n(shape_t::linear_operator(shape_t::vector(2), shape_t::vector(2)));
   n[0][0] = n[1][1] = 1;
   for (int i = 0; i < 10; ++i) n = compose(m, n);
   contract_assert((int)std::round(n[0][0].extract_number()) == 34);
@@ -29,11 +29,11 @@
 }
 
 [[= ivl::test]] void test_fib3() {
-  mdarray m(shaped, 2, 2);
+  mdarray m(shape_t::linear_operator(shape_t::vector(2), shape_t::vector(2)));
   m[1][1] = m[1][0] = m[0][1] = 1;
-  mdarray v(shaped, 2);
+  mdarray v(shape_t::vector(2));
   v[1] = 1;
-  for (int i = 0; i < 10; ++i) v = m(v.data);
+  for (int i = 0; i < 10; ++i) v = m(v);
   contract_assert((int)std::round(v[0].extract_number()) == 55);
   contract_assert((int)std::round(v[1].extract_number()) == 89);
 }
@@ -47,22 +47,22 @@ struct rnd_gen {
   }
   double get(double x) { return get(-x, x); }
   double get() { return get(1); }
-  mdarray getmd(double lo, double hi, shaped_t, auto... extents) {
-    mdarray ret(shaped, extents...);
+  mdarray getmd(double lo, double hi, const shape_t& shape) {
+    mdarray ret(shape);
     for (auto& el : ret.data) el = get(lo, hi);
     return ret;
   }
-  mdarray getmd(double x, shaped_t, auto... extents) { return getmd(-x, x, shaped, extents...); }
-  mdarray getmd(shaped_t, auto... extents) { return getmd(1, shaped, extents...); }
+  mdarray getmd(double x, const shape_t& shape) { return getmd(-x, x, shape); }
+  mdarray getmd(const shape_t& shape) { return getmd(1, shape); }
 };
 
 [[= ivl::test]] void test_tensor1() {
   rnd_gen g(1337);
-  auto m = g.getmd(10, shaped, 3, 4, 5);
-  auto v1 = g.getmd(shaped, 3);
-  auto v2 = g.getmd(shaped, 4);
-  auto v3 = g.getmd(shaped, 5);
-  auto a = m(v1.data)(v2.data)(v3.data).extract_number();
+  auto m = g.getmd(10, shape_t::linear_operator(shape_t::vector(3), shape_t::vector(4), shape_t::vector(5)));
+  auto v1 = g.getmd(shape_t::vector(3));
+  auto v2 = g.getmd(shape_t::vector(4));
+  auto v3 = g.getmd(shape_t::vector(5));
+  auto a = m(v1)(v2)(v3).extract_number();
   auto b = compose(v3, compose(v2, compose(v1, m))).extract_number();
   std::cerr << a << std::endl;
   std::cerr << b << std::endl;
@@ -71,17 +71,19 @@ struct rnd_gen {
 
 [[= ivl::test]] void test_tensor2() {
   rnd_gen g(4010101);
-  auto a = g.getmd(2, shaped, 3, 4, 5);
-  auto b = g.getmd(2, shaped, 5, 4, 3);
+  auto a = g.getmd(2, shape_t::linear_operator(shape_t::vector(3), shape_t::vector(4), shape_t::vector(5)));
+  auto b = g.getmd(2, shape_t::linear_operator(shape_t::vector(5), shape_t::vector(4), shape_t::vector(3)));
   auto c = compose(a, b);
-  contract_assert(c.rank() == 4);
-  contract_assert(c.shape == std::vector<std::size_t>{3, 4, 4, 3});
-  auto v1 = g.getmd(2, shaped, 3);
-  auto v2 = g.getmd(2, shaped, 4);
-  auto v3 = g.getmd(2, shaped, 4);
-  auto v4 = g.getmd(2, shaped, 3);
-  auto x = b(a(v1.data)(v2.data).data)(v3.data)(v4.data).extract_number();
-  auto y = c(v1.data)(v2.data)(v3.data)(v4.data).extract_number();
+  contract_assert(c.shape.linear_operator_rank() == 3);
+  contract_assert(
+    c.shape == shape_t::linear_operator(shape_t::vector(3), shape_t::vector(4), shape_t::vector(4), shape_t::vector(3))
+  );
+  auto v1 = g.getmd(2, shape_t::vector(3));
+  auto v2 = g.getmd(2, shape_t::vector(4));
+  auto v3 = g.getmd(2, shape_t::vector(4));
+  auto v4 = g.getmd(2, shape_t::vector(3));
+  auto x = b(a(v1)(v2))(v3)(v4).extract_number();
+  auto y = c(v1)(v2)(v3)(v4).extract_number();
   std::cerr << x << std::endl;
   std::cerr << y << std::endl;
   contract_assert(abs(x - y) < 1e-5);
