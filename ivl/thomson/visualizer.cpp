93457b5ff5cdc250161ff65afd0634f254d382e7
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-06-27 05:20:38 +0200

    save

--- ivl/thomson/visualizer.cpp
+++ ivl/thomson/visualizer.cpp
@@ -1,3 +1,5 @@
+#include "eval"
+#include "gradient"
 #include "point"
 #include <raylib/raylib.h>
 #include <raylib/raymath.h>
@@ -34,6 +36,82 @@ void draw_arc(Vector3 a, Vector3 b, Color color) {
   }
 }
 
+void draw_arrow(Vector3 from, Vector3 direction, Color color) {
+  auto to = Vector3Add(from, direction);
+  auto mid = Vector3Lerp(from, to, 0.75f);
+  auto delta = Vector3Scale(Vector3Normalize(Vector3CrossProduct(from, direction)), Vector3Length(direction) * 0.05f);
+  DrawLine3D(from, mid, color);
+  DrawTriangle3D(to, Vector3Add(mid, delta), Vector3Subtract(mid, delta), color);
+  DrawTriangle3D(to, Vector3Subtract(mid, delta), Vector3Add(mid, delta), color);
+}
+
+bool try_newton_fixup(std::span<point> points, double& ev) {
+  auto g = gradient2(points);
+  auto g2 = diff2(points);
+  double up = 0.0;
+  for (std::size_t i = 0; i < points.size(); ++i) up += dot(g[i], g[i]);
+  double down = 0.0;
+  for (std::size_t i = 0; i < points.size(); ++i)
+    for (std::size_t j = 0; j < points.size(); ++j)
+      for (int k = 0; k < 3; ++k)
+        for (int l = 0; l < 3; ++l) down += g2[i * points.size() + j][k][l] * g[i][k] * g[j][l];
+  double coef = -up / down;
+  // LOG(coef);
+  std::vector nxt(std::from_range, points);
+  for (std::size_t i = 0; i < points.size(); ++i) nxt[i] += coef * g[i];
+  auto nxt_ev = evaluate(nxt);
+  // if (nxt_ev > ev - 1e-5) return false;
+  for (std::size_t i = 0; i < points.size(); ++i) points[i] = nxt[i];
+  ev = nxt_ev;
+  normalize(points);
+  return true;
+}
+
+std::vector<point> fixup(std::span<const point> points, double* last = nullptr) {
+  auto g = gradient2(points);
+  for (int i = 0; i < points.size(); ++i) {
+    g[i] -= points[i] * dot(g[i], points[i]);
+  }
+  std::vector<point> copy(points.size());
+  auto mix = [&](double r) -> std::span<const point> {
+    for (std::size_t i = 0; i < points.size(); ++i) copy[i] = points[i] - r * g[i];
+    normalize(copy);
+    return copy;
+  };
+  double lo = 0;
+  double lo_e = evaluate_assume_normed(mix(lo));
+  double hi = 1; // last ? *last : 1;
+  double hi_e = evaluate_assume_normed(mix(hi));
+  // if (hi_e < lo_e - 1e-2) return mix(hi);
+  // while (true) {
+  //   double nhi = hi * 1.5;
+  //   double nhi_e = evaluate_assume_normed(mix(nhi));
+  //   if (nhi_e > hi_e - 1e-6) break;
+  //   hi = nhi;
+  //   hi_e = nhi_e;
+  // }
+  while ((hi - lo) > 1e-9) {
+    double mid = (hi + lo) / 2;
+    double mid_e = evaluate_assume_normed(mix(mid));
+    if (lo_e < hi_e) hi = mid, hi_e = mid_e;
+    else lo = mid, lo_e = mid_e;
+  }
+  // LOG(lo);
+  if (last) *last = lo;
+  mix(lo);
+  return copy;
+}
+
+bool try_gradient_fixup(std::span<point> points, double& ev, double* last = nullptr) {
+  auto nxt = fixup(points, last);
+  auto nxt_ev = evaluate(nxt);
+  if (nxt_ev > ev - 1e-5) return false;
+  for (std::size_t i = 0; i < points.size(); ++i) points[i] = nxt[i];
+  ev = nxt_ev;
+  normalize(points);
+  return true;
+}
+
 int ivl_main(std::size_t point_count) {
   const int screenWidth = 1000;
   const int screenHeight = 1000;
@@ -45,6 +123,7 @@ int ivl_main(std::size_t point_count) {
   std::vector<point> points(point_count);
   for (auto& p : points) p = random_point();
   normalize(points);
+  double ev = evaluate(points);
   SetConfigFlags(FLAG_WINDOW_HIGHDPI);
   InitWindow(screenWidth, screenHeight, "visualizer");
 
@@ -57,6 +136,13 @@ int ivl_main(std::size_t point_count) {
 
   SetTargetFPS(60);
   float rotation_speed = 1.0f;
+  int iteration = 0;
+  auto iterate = [&](std::size_t num) {
+    while (num--) {
+      ++iteration;
+      try_gradient_fixup(points, ev);
+    }
+  };
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
     if (IsKeyPressed(KEY_KP_ADD)) rotation_speed *= 2;
@@ -85,12 +171,26 @@ int ivl_main(std::size_t point_count) {
       camera.position = new_position;
       camera.up = new_up;
     }
+    // if (IsKeyPressed(KEY_ENTER)) {
+    //   ++iteration;
+    //   try_newton_fixup(points, ev);
+    // }
+    if (IsKeyPressed(KEY_ONE)) iterate(1);
+    if (IsKeyPressed(KEY_TWO)) iterate(10);
+    if (IsKeyPressed(KEY_THREE)) iterate(100);
+    if (IsKeyPressed(KEY_FOUR)) iterate(1000);
     BeginDrawing();
     ClearBackground(WHITE);
     BeginMode3D(camera);
-    draw_arc(point2vec(points[0]), point2vec(points[1]), BLUE);
+    // draw_arc(point2vec(points[0]), point2vec(points[1]), BLUE);
+    if (!IsKeyDown(KEY_P)) {
+      auto g = gradient2(points);
+      for (std::size_t i = 0; i < point_count; ++i) {
+        draw_arrow(point2vec(points[i]), point2vec(g[i]), BLUE);
+      }
+    }
     for (auto&& p : points) {
-      DrawSphere(point2vec(p), point_radius, point_color);
+      DrawSphereEx(point2vec(p), point_radius, 8, 8, point_color);
       // draw_circle(point2vec(p), 0.01f, point2vec(p), point_color);
       // DrawCircle3D(point2vec(p), 0.01f, point2vec(p), 0.0f, point_color);
     }
@@ -100,6 +200,8 @@ int ivl_main(std::size_t point_count) {
     EndMode3D();
     DrawFPS(10, 10);
     DrawText(std::format("rotation speed: {}", rotation_speed).c_str(), 10, 30, 30, GREEN);
+    DrawText(std::format("iteration: {}", iteration).c_str(), 10, 60, 30, GREEN);
+    DrawText(std::format("eval: {}", ev).c_str(), 10, 90, 30, GREEN);
     EndDrawing();
   }
   CloseWindow();
