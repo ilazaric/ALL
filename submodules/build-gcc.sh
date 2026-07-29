d32f4bd67c2b49cf7c457ed15c702d437b22e139
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-07-29 13:19:21 +0200

    build gcc script update

--- submodules/build-gcc.sh
+++ submodules/build-gcc.sh
@@ -1,12 +1,77 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x
 
-# COMMIT="$(git rev-parse HEAD:submodules/gcc)"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 OBJ_DIR="$SCRIPT_DIR/objdir/gcc"
 SRC_DIR="$SCRIPT_DIR/gcc"
+
+INSTALL=0
+CLEAN_FIRST=0
+ARG_ERRORS=0
+
+function print_help() {
+    echo './build-gcc.sh -- script to compile g++
+
+options:
+  -j NUM
+    number of processes allowed to run in parallel, defaults to $(nproc)
+
+  --install
+    run `make install` after successful build
+
+  --clean-first
+    fully purge objdir before building
+
+  --help
+    print this message and exit
+'
+}
+
+while [ "$#" -ne 0 ]
+do
+    OPTION="$1"
+    shift
+    case "$OPTION" in
+        "-j")
+            PARALLELISM="$1"
+            shift
+            ;;
+        "--install")
+            INSTALL=1
+            ;;
+        "--clean-first")
+            CLEAN_FIRST=1
+            ;;
+        "--help")
+            print_help
+            ARG_ERRORS=1
+            ;;
+        *)
+            echo "unknown argument: $OPTION"
+            ARG_ERRORS=1
+            ;;
+    esac
+done
+
+if [ $ARG_ERRORS -eq 1 ]
+then
+    exit 1
+fi
+
+set -x
+
+if [ $CLEAN_FIRST -eq 1 ]
+then
+    echo "purging objdir ..."
+    rm -rf "$OBJ_DIR"
+fi
+
+if [ -z ${PARALLELISM+x} ]
+then
+    PARALLELISM="$(nproc)"
+    echo "defaulting parallelism to $PARALLELISM"
+fi
 
 cd "$SRC_DIR"
 ./contrib/download_prerequisites
@@ -32,9 +97,9 @@ else
     cd "$OBJ_DIR"
 fi
 
-make -j $(nproc)
+make -j "$PARALLELISM"
 
-if [ "$#" -ge 1 ] && [ "$1" == "--install" ]
+if [ $INSTALL -eq 1 ]
 then
     echo "installing ..."
     make install
