3b94ebbc2fe5158c91436dec0866ae43efb4beed
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-07-30 14:57:42 +0200

    save

---
+++ ivl/parsing/ninja_emulate.cpp
@@ -0,0 +1,28 @@
+#include <ivl/parsing/ninja>
+#include <ivl/reflection/json>
+#include <filesystem>
+#include <print>
+#include <string>
+
+struct args {
+  std::filesystem::path C;
+  std::filesystem::path f = "build.ninja";
+  std::string t;
+};
+
+int ivl_main(const args& args, std::span<const char*> arg_targets) {
+  if (!args.C.empty()) current_path(args.C);
+  auto state = ivl::parsing::ninja::parse(args.f);
+  auto targets = arg_targets.empty() ? std::vector(std::from_range, state.defaults)
+                                     : std::vector<std::string>(std::from_range, arg_targets);
+  for (auto&& target : targets) contract_assert(state.targets.contains(target));
+  if (args.t == "commands") {
+    ivl::todo();
+  } else if (args.t == "targets") {
+    ivl::todo();
+  } else {
+    std::println("unknown command: {:?}", args.t);
+    return 1;
+  }
+  return 0;
+}
