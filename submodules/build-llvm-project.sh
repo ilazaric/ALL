aa2219c44d53a4db1f3cf07bf74bbd411fe48cee
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-07-30 14:55:01 +0200

    llvm-project build

--- submodules/build-llvm-project.sh
+++ submodules/build-llvm-project.sh
@@ -21,7 +21,7 @@ then
         -DLLVM_TARGETS_TO_BUILD=X86 \
         -DLLVM_ENABLE_EH=ON \
         -DLLVM_ENABLE_RTTI=ON \
-        -DLLVM_ENABLE_PROJECTS="clang;lld" \
+        -DLLVM_ENABLE_PROJECTS="clang;lld;bolt;clang-tools-extra" \
         -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_C_FLAGS_RELEASE="-O3" \
@@ -29,12 +29,13 @@ then
         -DCMAKE_CXX_STANDARD=23 \
         -GNinja \
         -S "$CMAKE_SRC_DIR" \
-        -B "$OBJ_DIR"
+        -B "$OBJ_DIR" \
+        -DCMAKE_INSTALL_PREFIX:PATH=/opt/llvm-project
 fi
 
 CGROUP_DIR="/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/compilation_group"
 mkdir -p "$CGROUP_DIR"
-echo "2000000 100000" > "$CGROUP_DIR/cpu.max"
+echo "1800000 100000" > "$CGROUP_DIR/cpu.max"
 echo "12G" > "$CGROUP_DIR/memory.max"
 echo "0" > "$CGROUP_DIR/memory.swap.max"
 echo "0" > "$CGROUP_DIR/memory.zswap.max"
