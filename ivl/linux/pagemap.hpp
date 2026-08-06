805d0394f16a40edf0635bfaf6af555d7fd7d168
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-08-06 23:02:11 +0200

    pagemap

--- ivl/linux/pagemap.hpp
+++ ivl/linux/pagemap.hpp
@@ -3,18 +3,36 @@
 #include <ivl/linux/file_descriptor>
 #include <ivl/linux/throwing_syscalls>
 
+// man proc_pid_pagemap
+
 namespace ivl::linux {
-uint64_t pagemap(const void* addr, file_descriptor fd) {
+struct pagemap_value {
+  uint64_t value;
+
+  uint64_t bit(uint64_t pos) { return (value >> pos) & 1; }
+
+  bool is_in_ram() const { return bit(63); }
+  bool is_in_swap() const { return bit(62); }
+  bool is_file_or_shared() const { return bit(61); }
+  // 60-58 reserved
+  bool is_write_protected() const { return bit(57); }
+  bool is_exclusive() const { return bit(56); }
+  bool is_soft_dirty() const { return bit(55); }
+
+  uint64_t page_frame_number() const { return value & ((1ULL << 55) - 1); }
+};
+
+pagemap_value pagemap(const void* addr, file_descriptor fd) {
   namespace sys = ivl::linux::throwing_syscalls;
   uint64_t loc = reinterpret_cast<uint64_t>(addr) / 4096 * 8;
   sys::lseek(fd.get(), loc, SEEK_SET);
   uint64_t ret;
   auto cnt = sys::read(fd.get(), (char*)&ret, 8);
   contract_assert(cnt == 8);
-  return ret;
+  return {ret};
 }
 
-uint64_t pagemap(const void* addr) {
+pagemap_value pagemap(const void* addr) {
   namespace sys = ivl::linux::throwing_syscalls;
   ivl::linux::owned_file_descriptor fd{sys::open("/proc/self/pagemap", O_RDONLY, 0)};
   return pagemap(addr, fd);
