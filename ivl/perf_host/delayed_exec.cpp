3a345a6b47ea7fa2ae16874d3a96d70317b80f4a
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-08-24 21:24:15 +0200

    save

--- ivl/perf_host/delayed_exec.cpp
+++ ivl/perf_host/delayed_exec.cpp
@@ -1,17 +1,17 @@
 #include <ivl/linux/terminate_syscalls>
 #include <span>
-#include <ivl/command_line_argument_parsing/raw_arguments>
+#include <ivl/command_line_argument_parsing/passthrough>
 
 // IVL add_compiler_flags("-static")
 
 struct args {};
 
-int ivl_main(args, ivl::cmdline_parsing::raw_arguments cmd) {
+int ivl_main(args, ivl::cmdline_parsing::passthrough cmd) {
   namespace sys = ivl::linux::terminate_syscalls;
-  contract_assert(cmd.size() >= 3);
-  auto ctl = cmd[0];
-  auto ctl_ack = cmd[1];
-  cmd.remove_prefix(2);
+  contract_assert(cmd.data.size() >= 3);
+  auto ctl = cmd.data[0];
+  auto ctl_ack = cmd.data[1];
+  cmd.data = cmd.data.subspan(2);
   auto ctl_fd = sys::open(ctl, O_WRONLY | O_CLOEXEC, 0);
   auto ctl_ack_fd = sys::open(ctl_ack, O_RDONLY | O_CLOEXEC, 0);
 
@@ -29,6 +29,6 @@ int ivl_main(args, ivl::cmdline_parsing::raw_arguments cmd) {
   auto len = sys::read(ctl_ack_fd, &out[0], 4);
   contract_assert(len == 4); // "ack\n"
   // printf("%s\n", out);
-  sys::execve(cmd.rest[0], &cmd.rest[0], nullptr /* TODO */);
+  sys::execve(cmd.data[0], &cmd.data[0], nullptr /* TODO */);
   return 0;
 }
