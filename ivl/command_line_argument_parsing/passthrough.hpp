c1e1f6f9184076eb34810263313665d13f439200
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-08-24 20:18:39 +0200

    passthrough

--- ivl/command_line_argument_parsing/passthrough.hpp
+++ ivl/command_line_argument_parsing/passthrough.hpp
@@ -18,6 +18,13 @@ struct parser<passthrough> {
       std::println("passthrough does not support `=value` argument style");
       return false;
     }
+    if (!rest.empty()) {
+      if (rest[0] == "--") rest.remove_prefix(1);
+      else if (rest[0].starts_with("--")) {
+        std::println("first argument to passthrough is option-like {:?}, add explicit \"--\" if not mistake", rest[0]);
+        return false;
+      }
+    }
     arg.data = rest.rest;
     rest.remove_prefix(rest.size());
     return true;
