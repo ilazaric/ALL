7e0f0505f3f88db03e05a2794ccedc484321a91e
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-08-25 22:05:54 +0200

    save

--- ivl/cpp-parser/grammar_utils.hpp
+++ ivl/cpp-parser/grammar_utils.hpp
@@ -9,6 +9,7 @@
 #include <optional>
 #include <type_traits>
 #include <vector>
+#include <variant>
 
 struct NewLine;
 template <typename>
--- ivl/cpp-parser/preprocessing.cpp
+++ ivl/cpp-parser/preprocessing.cpp
@@ -2,13 +2,13 @@
 
 using namespace ivl::cppp::grammar;
 
-struct ConditionallySupportedDirective : UnimplementedTODO {};
+struct ConditionallySupportedDirective : UnimplementedTODO<> {};
 
 struct GroupPart : Or<ControlLine, IfSection, TextLine, And<Literal<"#">, ConditionallySupportedDirective>> {};
 
 struct Group : Or<GroupPart, And<Group, GroupPart>> {};
 
-struct ModuleFile : UnimplementedTODO {};
+struct ModuleFile : UnimplementedTODO<> {};
 
 struct PreprocessingFile : Or<Opt<Group>, ModuleFile> {};
 
