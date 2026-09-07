2d2745aacfbe96136e8688b57cbba8d304b5c66c
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-07 16:22:21 +0200

    save

--- ivl/parsing/make.hpp
+++ ivl/parsing/make.hpp
@@ -14,25 +14,96 @@ struct variable_definition;
 struct directive;
 struct comment;
 
-struct explicit_rule {
-  std::string target;
-  std::vector<std::string> prerequisites;
-  std::string recipe;
+// struct explicit_rule {
+//   std::string target;
+//   std::vector<std::string> prerequisites;
+//   std::string recipe;
+// };
+
+struct variable_definition {
+  std::string name;
+  std::string contents;
+  bool recursively_expanded;
+  bool overriden; // either passed via cmdline (make A=1), or override A=1
+};
+
+struct variable_name_compare {
+  using is_transparent = void;
+  static std::string_view name(const variable_name_compare& v) { return a.name; }
+  static std::string_view name(const std::string& s) { return s; }
+  static std::string_view name(std::string_view sv) { return sv; }
+  static bool operator()(const auto& a, const auto& b) { return name(a) < name(b); }
 };
 
 struct state {
-  std::map<std::string, std::string, std::less<>> variables;
+  std::set<variable_definition, variable_name_compare> variables;
+  // std::map<std::string, std::string, std::less<>> variables;
 
   std::string dot_variable() const {
     std::string ret = ".VARIABLES";
-    for (auto&& [k, _] : variables) ret += " " + k;
+    for (auto&& var : variables) ret += " " + var.name;
+    return ret;
+  }
+
+  std::string expand_impl(std::string_view& text) {
+    contract_assert(text.starts_with("$("));
+    text.remove_prefix(2);
+    std::string ret;
+    while (true) {
+      text.empty() && panic("unterminated \"$(\"");
+      if (text.starts_with(')')) {
+        text.remove_prefix(1);
+        break;
+      }
+      // TODO
+      // if (text.starts_with(
+    }
+  }
+
+  std::string expand(std::string_view text) {
+    auto text_copy = text;
+    EXCEPTION_CONTEXT("text: {}", text_copy);
+    EXCEPTION_CONTEXT("at: {}", text.data() - text_copy.data());
+    std::string ret;
+    auto take = [&](size_t n) {
+      auto r = text.substr(0, n);
+      text.remove_prefix(n);
+      return r;
+    };
+    while (!text.empty()) {
+      if (text.starts_with("$$")) {
+        ret += "$";
+        text.remove_prefix(2);
+        continue;
+      }
+      if (text.starts_with("$(")) {
+        text.remove_prefix(2);
+        auto loc = text.find(')');
+        loc == std::string_view::npos&& panic("missing closing paren");
+        auto name = text.substr(0, loc);
+        name.find('\n') == std::string_view::npos || panic("newline before closing paren: {:?}", text_copy);
+        for (auto c : name) {
+          c == ':' && panic("variables cannot have ':' in name");
+          c == '#' && panic("variables cannot have '#' in name");
+          c == '=' && panic("variables cannot have '=' in name");
+          ::isspace(c) && panic("variables cannot have whitespace in name");
+        }
+        ret += get_variable(name);
+        text.remove_prefix(loc + 1);
+        continue;
+      }
+      text.starts_with("$") && todo();
+      ret += take(1);
+    }
     return ret;
   }
 
   std::string get_variable(std::string_view name) const {
     if (name == ".VARIABLES") return dot_variable();
     auto it = variables.find(name);
-    return it == variables.end() ? "" : it->second;
+    if (it == variables.end()) return "";
+    auto& var = *it;
+    return var.recursively_expanded ? expand(var.contents) : var.contents;
   }
 
   void set_variable_recursively_expanded(std::string_view name, std::string_view text) {
@@ -42,8 +113,96 @@ struct state {
   }
 };
 
-struct parser : basic_parser {
-  using basic_parser::basic_parser;
+struct logical_parser {
+  std::string logical_contents;
+  basic_parser logical_parser;
+  basic_parser physical_parser;
+  bool escaped = false;
+
+  struct checkpoint {
+    basic_parser::checkpoint logical;
+    basic_parser::checkpoint physical;
+  };
+
+  checkpoint get_checkpoint() const { return {logical_parser.get_checkpoint(), physical_parser.get_checkpoint()}; }
+
+  void restore_from_checkpoint(checkpoint c) {
+    logical_parser.restore_from_checkpoint(c.logical);
+    physical_parser.restore_from_checkpoint(c.physical);
+  }
+
+  logical_parser(std::string_view contents) : logical_parser(contents), physical_parser(contents) {
+    while (!contents.empty()) {
+      if (contents.starts_with("\\\n")) {
+        contents.remove_prefix(2);
+        continue;
+      }
+      if (contents.starts_with("\\\\")) {
+        contents.remove_prefix(2);
+        logical_contents += "\\\\";
+        continue;
+      }
+      logical_contents += contents[0];
+      contents.remove_prefix(1);
+    }
+    logical_parser = basic_parser(logical_contents);
+  }
+
+  bool finished() const { return logical_parser.finished(); }
+
+  decltype(auto) current_sv() const { return logical_parser.current_sv(); }
+  decltype(auto) current_c() const { return logical_parser.current_c(); }
+  decltype(auto) slice(size_t lo, size_t hi) const { return logical_parser.slice(lo, hi); }
+
+  decltype(auto) debug_context() const { return physical_parser.debug_context(); }
+  decltype(auto) debug_context_file(size_t count) const { return physical_parser.debug_context_file(count); }
+
+  void consume_c_nocheck() {
+    if (escaped) {
+      physical_parser.consume_c_nocheck();
+      logical_parser.consume_c_nocheck();
+      while (physical_parser.consume_if("\\\n"));
+      escaped = false;
+      return;
+    }
+    if (current_sv().starts_with("\\\\")) {
+      physical_parser.consume_c_nocheck();
+      logical_parser.consume_c_nocheck();
+      escaped = true;
+      return;
+    }
+    physical_parser.consume_c_nocheck();
+    logical_parser.consume_c_nocheck();
+    while (physical_parser.consume_if("\\\n"));
+  }
+
+  void consume_c(char c) {
+    current_c() == c || panic("tried to consume different character: arg={:?}, actual={:?}", c, current_c());
+    consume_c_nocheck();
+  }
+
+  bool consume_c_if(char c) {
+    if (!finished() && current_c() == c) {
+      consume_c(c);
+      return true;
+    } else return false;
+  }
+
+  void consume(std::string_view sv) {
+    for (auto c : sv) consume_c(c);
+  }
+
+  bool consume_if(std::string_view sv) {
+    if (!current_sv().starts_with(sv)) return false;
+    consume(sv);
+    return true;
+  }
+
+  size_t get_cursor() const { return logical_parser.cursor; }
+};
+
+struct parser : logical_parser {
+  using logical_parser::logical_parser;
 
   bool consume_if_ws() {
     if (finished()) return false;
@@ -57,14 +216,20 @@ struct parser : basic_parser {
 
   void consume_while_ws() { while (consume_if_ws()); }
 
+  std::string_view consume_logical_line() {
+    auto start = get_cursor();
+    while (!finished()) {
+      // if (consume_if("\\\n")) continue;
+      // if (consume_if("\\\\")) continue;
+      if (consume_c_if("\n")) break;
+      consume_c_nocheck();
+    }
+    return slice(start, get_cursor());
+  }
+
   void parse_comment() {
     consume_c('#');
-    while (true) {
-      while (current_c() != '\n') consume_c_nocheck();
-      consume_c('\n');
-      if (contents[cursor - 2] == '\\') continue;
-      break;
-    }
+    consume_logical_line();
   }
 
   bool try_parse_comment() {
@@ -134,20 +299,20 @@ struct parser : basic_parser {
     consume_c('(');
     std::string_view arg1, arg2;
     {
-      auto arg1_begin = cursor;
+      auto arg1_begin = get_cursor();
       while (!consume_c_if(',')) {
         current_c() == '\n' && panic("malformed ifeq");
         if (current_sv().starts_with("$(")) stupid_consume_dollar_paren();
         else consume_c_nocheck();
       }
-      auto arg1_end = cursor - 1;
-      auto arg2_begin = cursor;
+      auto arg1_end = get_cursor() - 1;
+      auto arg2_begin = get_cursor();
       while (!consume_c_if(')')) {
         current_c() == '\n' && panic("malformed ifeq");
         if (current_sv().starts_with("$(")) stupid_consume_dollar_paren();
         else consume_c_nocheck();
       }
-      auto arg2_end = cursor - 1;
+      auto arg2_end = get_cursor() - 1;
       arg1 = slice(arg1_begin, arg1_end);
       arg2 = slice(arg2_begin, arg2_end);
       while (!arg1.empty() && ::isspace(arg1.back())) arg1.remove_suffix(1);
@@ -190,13 +355,30 @@ struct parser : basic_parser {
 
   bool try_parse_variable_assignment(state& global_state) {
     auto cp = get_checkpoint();
+    // bool revert = true;
+    // util::scope_exit _{[&]{ if (revert) restore_from_checkpoint(cp); }};
     consume_while_ws();
+    if (finished()) return (restore_from_checkpoint(cp), false);
     if (current_c() == '\n') return (restore_from_checkpoint(cp), false);
     if (current_c() == '#') return (restore_from_checkpoint(cp), false);
     if (current_c() == ':') return (restore_from_checkpoint(cp), false);
-    auto name_begin = cursor;
+    auto name_begin = get_cursor();
+    std::string_view op;
+    auto try_parse_op = [&] {
+      auto begin = get_cursor();
+      if (
+        consume_if("=") || consume_if("+=") || consume_if(":=") || consume_if("::=") || consume_if("?=") ||
+        consume_if("!=")
+      )
+        return false;
+      op = slice(begin, get_cursor());
+      return true;
+    };
     while (true) {
+      if (finished()) return (restore_from_checkpoint(cp), false);
       if (::isspace(current_c())) break;
+      if (try_parse_op()) {
+      }
       if (current_c() == '=') break;
       if (current_sv().starts_with("+=")) break;
       if (current_sv().starts_with(":=")) break;
@@ -206,7 +388,7 @@ struct parser : basic_parser {
       if (current_c() == ':') return (restore_from_checkpoint(cp), false);
       consume_c_nocheck();
     }
-    auto name_end = cursor;
+    auto name_end = get_cursor();
     auto name = slice(name_begin, name_end);
     consume_while_ws();
     bool plus_eq = consume_if("+=");
@@ -214,9 +396,23 @@ struct parser : basic_parser {
       return (restore_from_checkpoint(cp), false);
     name.empty() && panic("empty variable name");
     std::string text;
-    while (true) {
+    while (!finished()) {
       if (consume_c_if('\n')) break;
-      if (consume_if("\\\n")) continue;
+      // if (consume_if("\\\n")) continue;
+      if (consume_if("\\\\")) {
+        text += "\\";
+        continue;
+      }
+      if (consume_if("\\#")) {
+        text += "#";
+        continue;
+      }
+      if (current_sv() == "\\") {
+        text += "\\";
+        consume_c('\\');
+        break;
+      }
+      current_c() == '\\' && todo();
       if (try_parse_comment()) break;
       text += current_c();
       consume_c_nocheck();
