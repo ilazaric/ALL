aa5d2ca89369e5c569ce4b0e2b5a2cd0d892c21f
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-12 09:52:28 +0200

    save

---
+++ ivl/experiments/scrapingcourse/example/script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S uv run
+
+from seleniumbase import SB
+
+# Log in to Swag Labs and save cookies
+with SB(uc=True) as sb:
+    sb.open("https://www.saucedemo.com")
+    sb.wait_for_element("div.login_logo")
+    sb.type("#user-name", "standard_user")
+    sb.type("#password", "secret_sauce")
+    sb.click('input[type="submit"]')
+    sb.highlight("div.inventory_list", loops=6)
+    sb.save_cookies(name="cookies.txt")
+
+# Load previously saved cookies to bypass login
+with SB(uc=True) as sb:
+    sb.open("https://www.saucedemo.com")
+    sb.load_cookies(name="cookies.txt")
+    sb.open("https://www.saucedemo.com/inventory.html")
+    sb.highlight("div.inventory_list", loops=12)
+    sb.save_screenshot_to_logs()
