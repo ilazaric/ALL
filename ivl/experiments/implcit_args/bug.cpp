38925bc9a7988a33ec8548a6d62ecbf362e6b268
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-12 04:22:39 +0200

    save

--- ivl/experiments/implcit_args/bug.cpp
+++ ivl/experiments/implcit_args/bug.cpp
@@ -1,33 +1,19 @@
 #include <meta>
-#include <string_view>
 
-template<typename T>
-struct implicit_parsed_storage {
-  // TOOD: gcc bug here?
+template<typename>
+struct storage {
   static inline int value = 2;
 };
 
-consteval int* find_value(std::meta::info storage) {
-  for (auto member : members_of(storage, std::meta::access_context::unchecked())) {
+consteval int* find_value(std::meta::info type) {
+  for (auto member : members_of(type, std::meta::access_context::unchecked())) {
     if (!has_identifier(member)) continue;
     if (identifier_of(member) != "value") continue;
     return &extract<int&>(member);
   }
-  contract_assert(false);
+  throw;
 }
 
-template<typename T>
-struct implicit_name {
-    int* value_ptr;
-
-  consteval implicit_name() {
-    value_ptr = find_value(^^implicit_parsed_storage<bool>);
-  }
-};
-
-// IVL add_compiler_flags("-Wno-non-template-friend -Wsfinae-incomplete=0")
-
-int main() {
-  return *implicit_name<bool>().value_ptr;
-  // return implicit_flag("foo");
+int main() { //
+  return *find_value(^^storage<bool>);
 }
