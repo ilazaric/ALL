7a82caa80bcbc37f52782d2bdb51be429695e6b2
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-12 13:29:34 +0200

    touchup bool parsing

--- ivl/command_line_argument_parsing/parsers/bool.hpp
+++ ivl/command_line_argument_parsing/parsers/bool.hpp
@@ -6,45 +6,31 @@
 #include <string_view>
 
 namespace ivl::cmdline_parsing {
-/* TODO:
-   from gdb manual:
+/* from gdb manual:
    > Some options are described as accepting an argument which can be either on or off.
    > These are known as boolean options. Similarly to boolean settings commands—on and off
    > are the typical values, but any of 1, yes and enable can also be used as “true” value, and
    > any of 0, no and disable can also be used as “false” value. You can also omit a “true”
    > value, as it is implied by default.
 
-   maybe add on,yes,enable as well */
+   added on,yes,enable as well */
 template<>
 struct parser<bool> {
-  inline bool parse_one(bool& arg, std::string_view sv) const {
-    if (sv == "1" || sv == "true") {
+  // TODO: maybe booleans should only be settable with `--foo=bar` syntax, think more
+  inline bool parse(bool& arg, raw_arguments& rest) const {
+    if (rest.empty()) return arg = true;
+    std::string_view sv = rest[0];
+    if (sv == "1" || sv == "true" || sv == "on" || sv == "yes" || sv == "enable") {
+      rest.remove_prefix(1);
       arg = true;
       return true;
     }
-    if (sv == "0" || sv == "false") {
+    if (sv == "0" || sv == "false" || sv == "off" || sv == "no" || sv == "disable") {
+      rest.remove_prefix(1);
       arg = false;
       return true;
     }
-    // TODO: we should be able to remove this and add a generic top-level version?
-    std::println("failed to parse boolean, argument: {:?}", sv);
-    return false;
-  }
-
-  inline bool parse(bool& arg, raw_arguments& rest) const {
-    // TODO: pretty sure this is stupid, change/remove, i think just --boolean[=value] is enough
-    // ....: does that imply all optional args should only support `=value` parsing?
-    // UPDT: this todo is kinda broken since i killed opt<sv> argument
-    if (!rest.empty()) {
-      std::string_view nxt = rest[0];
-      if (!nxt.starts_with("--")) {
-        rest.remove_prefix(1);
-        return parse_one(arg, nxt);
-      }
-    }
-
-    arg = true;
-    return true;
+    return arg = true;
   }
 };
 } // namespace ivl::cmdline_parsing
