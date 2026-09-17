f4e05dad5a33affceea2e53c8a224061200b587a
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-17 06:59:12 +0200

    stl format compiles

--- ivl/stl/string_example.cpp
+++ ivl/stl/string_example.cpp
@@ -1,10 +1,10 @@
 #include <ivl/stl/string>
 #include <iostream>
-#include <print>
+#include <ivl/format>
 
 // IVL add_compiler_flags("-fsanitize=undefined,address")
 
 int main() {
   for (auto s : ivl::split_py_range(std::string("   hello    world  123   ")))
-    std::println("[{}]", s);
+    ivl::fmt::println("[{}]", s);
 }
