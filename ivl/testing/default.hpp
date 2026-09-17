3c82f308386e23095a83f337944572432be08f25
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-17 06:58:47 +0200

    testing format compiles

--- ivl/testing/default.hpp
+++ ivl/testing/default.hpp
@@ -3,7 +3,7 @@
 #include <ivl/reflection/json>
 #include <source_location>
 #include <string_view>
-#include <print>
+#include <ivl/format>
 
 namespace ivl::testing {
 inline void contract_assert_json(
@@ -12,10 +12,10 @@ inline void contract_assert_json(
   auto actual_json = ivl::to_json(actual);
   auto expected_json = nlohmann::json::parse(expected);
   if (actual_json != expected_json) {
-    std::println(stderr, "!!! ERROR: FAILED CHECK AT {}:{}", loc.file_name(), loc.line());
-    std::println(stderr, "actual:\n{}", actual_json.dump(2));
-    std::println(stderr, "expected:\n{}", expected_json.dump(2));
-    std::println(stderr, "diff:\n{}", nlohmann::json::diff(actual_json, expected_json).dump(2));
+    ivl::fmt::println(stderr, "!!! ERROR: FAILED CHECK AT {}:{}", loc.file_name(), loc.line());
+    ivl::fmt::println(stderr, "actual:\n{}", actual_json.dump(2));
+    ivl::fmt::println(stderr, "expected:\n{}", expected_json.dump(2));
+    ivl::fmt::println(stderr, "diff:\n{}", nlohmann::json::diff(actual_json, expected_json).dump(2));
   }
   contract_assert(actual_json == expected_json);
 }
