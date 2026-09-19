fe6fc20c934ddda512e711182c94b14278119cde
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-19 03:16:54 +0200

    fake fix

--- ivl/experiments/fs_path/format/bench_format.cpp
+++ ivl/experiments/fs_path/format/bench_format.cpp
@@ -1,7 +1,8 @@
-#error "broken since gcc has path formatting, TODO"
-
 #include <benchmark/benchmark.h>
 #include FORMAT_VERSION
+
+// just to get syntax_only test to pass, runit.sh is actual compilation
+// IVL add_compiler_flags("-DFORMAT_VERSION=\"ilazaric\"")
 
 // IVL add_compiler_flags_tail("-lbenchmark")
 
@@ -12,7 +13,7 @@ static void PathFormat(benchmark::State& state) {
                           "/home/dotdot/some/dot/really/dotdot/dot/dotdot/long/path"
                           "/home/dotdot/some/dot/really/dotdot/dot/dotdot/long/path"};
   for (auto _ : state) {
-    benchmark::DoNotOptimize(std::format("{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}", p));
+    benchmark::DoNotOptimize(std::format("{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}", *(ivl::path*)&p));
   }
 }
 BENCHMARK(PathFormat);
@@ -24,7 +25,7 @@ static void PathWideFormat(benchmark::State& state) {
                           "/home/dotdot/some/dot/really/dotdot/dot/dotdot/long/path"
                           "/home/dotdot/some/dot/really/dotdot/dot/dotdot/long/path"};
   for (auto _ : state) {
-    benchmark::DoNotOptimize(std::format(L"{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}", p));
+    benchmark::DoNotOptimize(std::format(L"{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}{0}", *(ivl::path*)&p));
   }
 }
 BENCHMARK(PathWideFormat);
