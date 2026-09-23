ee07f08ca041b80a8f574a5f9aebc4003991f2d3
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-23 17:23:05 +0200

    save

--- ivl/factorio/calc.cpp
+++ ivl/factorio/calc.cpp
@@ -13,12 +13,12 @@ T& unmove(T&& t) {
 }
 
 // const json recipes = json::parse(std::ifstream{"recipes.json"});
-const json recipes = json::parse(std::ifstream{"factorio-recipes.json"});
+const json recipes = boost::json::parse(std::ifstream{"factorio-recipes.json"});
 // const json all = json::parse(std::ifstream{"all.json"});
 
 int main() {
   // std::cout << dump(recipes, 2) << std::endl;
   // LOG(recipes[0]);
-  for (auto& el : recipes)
-    LOG(el["name"]);
+  for (auto& el : recipes.as_array())
+    LOG(el.as_object().at("name"));
 }
--- ivl/parsing/ninja/example.cpp
+++ ivl/parsing/ninja/example.cpp
@@ -1,7 +1,7 @@
 #include <ivl/parsing/ninja>
-#include <ivl/reflection/boost_json>
+#include <ivl/reflection/json>
 
 int ivl_main(const std::filesystem::path& p) {
-  ivl::fmt::println("{:2}", ivl::boosty::to_json(ivl::parsing::ninja::parse(p)));
+  ivl::fmt::println("{:2}", ivl::to_json(ivl::parsing::ninja::parse(p)));
   return 0;
 }
