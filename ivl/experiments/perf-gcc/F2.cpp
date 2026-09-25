4b9adcc58c22a8ca741174b2c0244baf1bc97c80
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-25 03:22:09 +0200

    save

--- ivl/experiments/perf-gcc/F2.cpp
+++ ivl/experiments/perf-gcc/F2.cpp
@@ -5,24 +5,20 @@
 
 struct Mint {
   std::array<uint32_t, 1> data;
-
-  friend constexpr Mint operator*(const Mint& a, const Mint& b) {
-    return Mint{a.data[0] + b.data[0]};
-  }
 };
 
 auto factorials_storage = [] {
   std::array<Mint, 20'005> out{};
   out[0] = 1;
-  for (uint32_t i = 1u; i < out.size(); ++i) out[i] = out[i - 1] * Mint{i};
+  for (uint32_t i = 1u; i < out.size(); ++i) out[i] = Mint{out[i - 1].data[0] * Mint{i}.data[0]};
   return out;
 }();
 
 /*
 ver == 14.4.0
-IVL: constexpr_ops_count: 2960625
+IVL: constexpr_ops_count: 2620557
 ver == 15.1.0
-IVL: constexpr_ops_count: 4560945
+IVL: constexpr_ops_count: 4220877
 ver == 16.1.0
-IVL: constexpr_ops_count: 4560945
+IVL: constexpr_ops_count: 4220877
  */
