ba04190e216145b281ec9dce368604cff8e20771
Ivan Lazaric <ivan.lazaric1@gmail.com>
2026-09-26 17:48:48 +0200

    save

---
+++ ivl/experiments/perf-gcc/F2v3-summarizer.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RUNCOUNT=5
+
+function compile_check() {
+    g++ F2v3.cpp -DCHOICE="$1" -fconstexpr-ops-limit=1000000000000 -std=c++23 -o "F2v3.$1" \
+        && echo "compiles: PASS" || { echo "compiles: FAIL"; return 1; }
+}
+
+function runtime_check() {
+    bash -c "./F2v3.$1 && exit 0 || exit 1" &> /dev/null \
+        && echo "runtime out-of-bounds test: FAIL"  \
+        || echo "runtime out-of-bounds test: PASS"
+}
+
+function perf_sanity_check() {
+    [ "$(cat "F2v3.$1.out" | grep "EXIT CODE: 0" | wc -l)" -eq "$RUNCOUNT" ] || \
+        { echo "perf sanity check: FAIL"; return 1; }
+    [ "$(cat "F2v3.$1.out" | grep "IVL" | uniq | wc -l)" -eq "1" ] || \
+        { echo "perf sanity check: FAIL"; return 1; }
+    echo "perf sanity check: PASS"
+}
+
+for CHOICE in BASELINE SEMICOLON STDLIB_{REL,DBG{,_V{2,3,4,5,6}}} IVL_{CONSTEVAL,ALWAYS{,_SIZE,_COND},BUILTIN}
+do
+    echo
+    echo "evaluating CHOICE=$CHOICE ..."
+    compile_check "$CHOICE" || continue
+    runtime_check "$CHOICE"
+    ivl script perf-run --copy F2v3.cpp --repeat "$RUNCOUNT" -- \
+        IVL_GCC_DUMP_CONSTEXPR_OPS_COUNT= \
+        g++ F2v3.cpp -DCHOICE="$CHOICE" -fsyntax-only -fconstexpr-ops-limit=1000000000000 -std=c++23 \
+        > "F2v3.$CHOICE.out"
+    perf_sanity_check "$CHOICE" || continue
+    OPS="$(cat "F2v3.$CHOICE.out" | grep IVL | head -1 | rev | cut -d ' ' -f 1 | rev)"
+    DUR="$(cat "F2v3.$CHOICE.out" | grep '^user'$'\t' \
+        | cut -d $'\t' -f 2 | cut -c 3- | rev | cut -c 2- | rev \
+        | awk '{s+=$1}END{print s/NR}')"
+    echo "${DUR}s, ${OPS}ops"
+done
+echo
--- ivl/experiments/perf-gcc/F2v3.cpp
+++ ivl/experiments/perf-gcc/F2v3.cpp
@@ -1,9 +1,82 @@
-#include <bits/c++config.h>
+#define _GLIBCXX_DEBUG
+#include <array> // just for the debug stuff, not std::array
 
 // IVL disable_ivl_main_handler()
 // IVL add_compiler_flags("-fconstexpr-ops-limit=1000000000000")
 
-#define __glibcxx_requires_subscript(_N) __glibcxx_assert(_N < this->size())
+namespace std
+{
+  __attribute__((__always_inline__,__visibility__("default")))
+  inline void
+  __glibcxx_assert_fail()
+  { }
+}
+
+# define __glibcxx_assert_rel(cond)					\
+  do {									\
+    if (std::__is_constant_evaluated() && !bool(cond))			\
+      std::__glibcxx_assert_fail();					\
+  } while (false)
+
+# define __glibcxx_assert_dbg(cond)					\
+  do {									\
+    if (__builtin_expect(!bool(cond), false))				\
+      _GLIBCXX_ASSERT_FAIL(cond);					\
+  } while (false)
+
+// 3.1574s, 22173301ops
+#define BASELINE
+
+// 3.1572s, 22173301ops
+#define SEMICOLON ;
+
+// 4.4912s, 35568562ops
+#define STDLIB_REL __glibcxx_assert_rel(n < this->size());
+
+// 5.0498s, 38438969ops
+#define STDLIB_DBG __glibcxx_check_subscript(n);
+
+// 5.0606s, 38438969ops
+#define STDLIB_DBG_V2                                                   \
+  do {									\
+    if (__builtin_expect(!bool(n < this->size()), false))               \
+      _GLIBCXX_ASSERT_FAIL(cond);					\
+  } while (false);
+
+// 4.991s, 37482165ops
+#define STDLIB_DBG_V3                                                   \
+  if (__builtin_expect(!bool(n < this->size()), false))                 \
+    _GLIBCXX_ASSERT_FAIL(cond);
+
+// 4.9898s, 37482165ops
+#define STDLIB_DBG_V4                                                   \
+  if (__builtin_expect(n >= this->size(), false))                       \
+    _GLIBCXX_ASSERT_FAIL(cond);
+
+// 4.0894s, 30784537ops
+#define STDLIB_DBG_V5                                                   \
+  if (__builtin_expect(n >= N, false))                                  \
+    _GLIBCXX_ASSERT_FAIL(cond);
+
+// 3.502s, 27914125ops
+#define STDLIB_DBG_V6                                                   \
+  if (n >= N)                                                           \
+    _GLIBCXX_ASSERT_FAIL(cond);
+
+// 3.385s, 26957321ops
+#define IVL_CONSTEVAL if not consteval { STDLIB_DBG }
+
+// 3.486s, 27914125ops
+#define IVL_ALWAYS if (n >= N) { STDLIB_DBG }
+
+// 4.3056s, 34611753ops
+#define IVL_ALWAYS_SIZE if (n >= this->size()) { STDLIB_DBG }
+
+// 4.2978s, 34611753ops
+#define IVL_ALWAYS_COND if (!bool(n <= this->size())) { STDLIB_DBG }
+
+// 3.4878s, 27914125ops
+#define IVL_BUILTIN if (!__builtin_is_constant_evaluated()) { STDLIB_DBG }
 
 template<typename T, unsigned N>
 struct array {
@@ -12,7 +85,7 @@ struct array {
   constexpr unsigned size() const { return N; }
 
   constexpr T& operator[](unsigned n) {
-    __glibcxx_requires_subscript(n);
+    CHOICE
     return elems[n];
   }
 };
@@ -27,3 +100,8 @@ constexpr auto storage = [] {
     for (unsigned j = 1; j < N; ++j) out[i][j] = out[i - 1][j] + out[i][j - 1];
   return out;
 }();
+
+int main() {
+  array<unsigned, 5> a;
+  a[10];
+}
